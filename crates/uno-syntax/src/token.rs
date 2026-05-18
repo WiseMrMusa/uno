820fc0601f5ef00eda240944b27c2b0e8b1ae5ce
Musa AbdulKareem <wisemrmusa@gmail.com>
2026-05-18 17:00:30 +0100

    feat: add token definitions"

---
+++ crates/uno-syntax/src/token.rs
@@ -0,0 +1,73 @@
+use crate::span::Span;
+
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
+
+pub enum Tokenkind {
+    Fn,
+    Let,
+    Return,
+    If,
+    Else,
+    True,
+    False,
+    Mut,
+    Pub,
+    While,
+    Loop,
+    For,
+    In,
+    Match,
+    Struct,
+    Enum,
+    Impl,
+    Use,
+
+    BoolType,
+    Uint(usize),
+
+    Integer(String),
+    Ident(String),
+
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    Equals,
+    EqualsEquals,
+    NotEquals,
+    Less,
+    Greater,
+    LessEquals,
+    GreaterEquals,
+    Not,
+    AndAnd,
+    OrOr,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    Comma,
+    Semicolon,
+    Colon,
+    Arrow,
+    Dot,
+    Underscore,
+    Hash,
+
+    Error(String),
+    Eof,
+    
+}
