a66c94ba651e5d0aef2db923f6c3b3733ed2a343
Musa AbdulKareem <wisemrmusa@gmail.com>
2026-05-18 16:39:39 +0100

    feat: implement span

---
+++ crates/uno-syntax/src/span.rs
@@ -0,0 +1,35 @@
+#[derive(SpanTraits)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+trait SpanTraits {
+    fn new(start: usize, end: usize) -> Self;
+    fn empty() -> Self;]
+    fn len(&self) -> Self;
+    fn is_empty(&self) -> bool;
+    fn merge(a: Span, b: Span) -> Self;
+}
+
+impl SpanTraits for Span {
+    pub fn new(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+
+    pub fn empty() -> Self {
+        Self { start: 0, end: 0}
+    }
+
+    pub fn len(&self) -> Self {
+        self.end.saturating_sub(self.start)
+    }
+
+    pub fn is_empty() -> bool {
+        self.start == self.end
+    }
+
+    pub fn merge(a: Span, b: Span) -> Span {
+        Span::new(a.start.min(b.start), a.end.max(b.end))
+    }
+}
